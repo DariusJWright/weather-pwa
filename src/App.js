e026bd44b44172a79156d6f8f3060c2562d70aaa
darshnablah <dwright758@gmail.com>
2020-12-20 19:29:47 -0600

    added logic to get weather data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { getWeather, getFiveDay } from './api/getWeather';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const [fiveDay, setFiveDay] = useState({});
+
+  const search = async (event) => {
+    event.preventDefault();
+    const data = await getWeather(query);
+    setWeather(data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <form id='form' onSubmit={search}>
+        <input
+          type='text'
+          className='search'
+          placeholder='Enter city name...'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type='submit'>Search</button>
+      </form> 
+      {weather.sys && (
+        <div className='weather'>
+          <h1 className='city'>{weather.name}, {weather.sys.country}</h1>
+          <h2 className='coord'>{`Lat: ${weather.coord.lat}, Lon: ${weather.coord.lon}`}</h2>
+          <h2 className='temp'>
+            {Math.round(weather.main.temp)}
+            <sup>&deg;F</sup>
+          </h2>
+          <div className='weather-info'>
+            <img className='icon' src={`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt={weather.weather[0].description} />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/api/getWeather.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { useImperativeHandle } from 'react';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const fiveDayURL = 'api.openweathermap.org/data/2.5/forecast'
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+export const getWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: 'imperial',
+      APPID: API_KEY
+    }
+  });
+
+  return data
+}
+
+export const getFiveDay = async (query) => {
+  const { data } = await axios.get(fiveDayURL, {
+    params: {
+      q: query,
+      units: useImperativeHandle,
+      APPID: API_KEY
+    }
+  });
+
+  return data;
+}
